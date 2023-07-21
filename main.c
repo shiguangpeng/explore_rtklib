379a2a85b8c08160279cff052cd2aa1d8e95ad23
shiguangpeng <shiguangpeng@ucen.cc>
2023-07-21 18:44:27 +0800

    没有完成修改

--- main.c
+++ main.c
@@ -2,13 +2,17 @@
 
 
 int main(void){
-    obsd_t *obs = malloc(sizeof(obsd_t));
-    int n = 1;
-    nav_t *nav = malloc(sizeof(nav_t));
-    prcopt_t *opt = malloc(sizeof(prcopt_t));
-    sol_t *sol = malloc(sizeof(sol_t));
-    double az[2] = {1, 3};
-    double *azel = az;
-    ssat_t *ssat = malloc(sizeof(ssat_t));
-    // pntpos();
+    // 初始化
+    obs_t *obs = (obs_t *)malloc(sizeof(obs_t));
+    nav_t *nav = (nav_t *)malloc(sizeof(nav_t));
+    sta_t *sat = (sta_t *)malloc(sizeof(sta_t));
+    const char *path = "F:\\work/gpsposition\\explore_rtklib\\data\\MAO000USA_R_20231980000_01D_MN.rnx";
+
+    // rcv=2为固定站，char* opt是一个参数，系统代码system mask（rinex.c -> Line 892），传空值
+    int status = readrnx(path, 2, "", NULL, nav, NULL);
+    if(status){
+        printf("%d", obs->data->rcv);
+    }else{
+        printf("no data!");
+    }
 }
